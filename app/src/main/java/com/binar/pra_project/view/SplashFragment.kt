3b4328266ce056c813fd3e8bbd33a36c9d83a44b
ASUS-PC <prcllputri1752@gmail.com>
2023-05-23 20:39:38 +0700

    pra

--- app/src/main/java/com/binar/pra_project/view/SplashFragment.kt
+++ app/src/main/java/com/binar/pra_project/view/SplashFragment.kt
@@ -1,24 +1,55 @@
 package com.binar.pra_project.view
 
+//import com.binar.pra_project.UserViewModel
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.binar.pra_project.R
 import com.binar.pra_project.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
     private lateinit var binding : FragmentSplashBinding
+//    private lateinit var userVM : UserViewModel
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
 
+    companion object{
+        const val PREFS_NAME = "dataUser"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentSplashBinding.inflate(layoutInflater,container, false)
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //userVM = ViewModelProvider(requireActivity()).get(UserViewModel::class.java)
+
+        sharedPreferences = requireActivity().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            navigateToHome()
+        }, 2000)
+
+    }
+
+    fun navigateToHome(){
+        findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
     }
 
 
